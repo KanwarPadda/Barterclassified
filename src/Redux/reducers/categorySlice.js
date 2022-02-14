fa3d4b4e11b71318be5a1b6e150792fa6bc1cbd4
IshtiaqueNafis <nafu22@gmail.com>
2022-02-14 11:46:21 -0500

    did firestore Init to setup hosting and sotrage.

--- src/Redux/reducers/categorySlice.js
+++ src/Redux/reducers/categorySlice.js
@@ -1,25 +1,24 @@
 import {createAsyncThunk, createEntityAdapter, createSlice} from "@reduxjs/toolkit";
 import {category} from "../constant/constant";
-import firebase from '../../firestore/firebase';
-import {dataFromSnapshot} from "../../firestore/fireStoreService";
+
 import {asyncActionError} from "./asyncSlice";
 
-const db = firebase.firestore();
+
 
 export const fetchCategories = createAsyncThunk(
     `${category}/loadCategories`,
     async (_, thunkApi) => {
 
         try {
-            const querySnapShot = await db.collection('Categories').get();
-            const categories = (querySnapShot.docs.map(d => (dataFromSnapshot(d))));
-            if (categories.length > 0) {
-                return categories;
-
-            } else {
-                return thunkApi.rejectWithValue({error: 'error'});
-
-            }
+            // const querySnapShot = await db.collection('Categories').get();
+            // const categories = (querySnapShot.docs.map(d => (dataFromSnapshot(d))));
+            // if (categories.length > 0) {
+            //     return categories;
+            //
+            // } else {
+            //     return thunkApi.rejectWithValue({error: 'error'});
+            //
+            // }
 
         } catch (error) {
             return thunkApi.rejectWithValue({error: error.data});
@@ -33,24 +32,8 @@ export const fetchCategories = createAsyncThunk(
 export const fetchSingleCategory = createAsyncThunk(
     `${category}/fetchSingleCategory`,
     async ({id}, thunkApi) => {
-        console.log({id});
-        try {
-            const snapshot = await db.collection('Categories').doc(id).get().then(snapshot => snapshot.data());
 
 
-            if (snapshot === undefined) {
-                thunkApi.dispatch(asyncActionError());
-                return thunkApi.rejectWithValue({error: 'error'});
-
-            }else {
-                thunkApi.dispatch(fetchCategories())
-                return snapshot.id;
-            }
-
-
-        } catch (error) {
-            return thunkApi.rejectWithValue({error: 'error'});
-        }
     }
 )
 
---
+++ src/firestore/config.js
@@ -0,0 +1,20 @@
+import firebase from "firebase/compat/app";
+import 'firebase/compat/firestore'
+import 'firebase/compat/database'
+import 'firebase/compat/auth'
+import 'firebase/compat/storage'
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDZNIREaPQy7sYAMK2iFGtwC6pBUhuUb8Y",
+    authDomain: "barterclassified.firebaseapp.com",
+    projectId: "barterclassified",
+    storageBucket: "barterclassified.appspot.com",
+    messagingSenderId: "921263841839",
+    appId: "1:921263841839:web:a9bbc512ae9ebe8b231cda"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const projectFireStore = firebase.firestore();
+export const projectAuth = firebase.auth();
+
