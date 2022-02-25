0f7f59b82617519e9c0646952cf3f8636526aed0
IshtiaqueNafis <nafu22@gmail.com>
2022-02-25 02:48:33 -0500

    intregrated stripe

--- src/components/layout/posts/postDetails/PostDetails.jsx
+++ src/components/layout/posts/postDetails/PostDetails.jsx
@@ -5,6 +5,8 @@ import {useDispatch, useSelector} from "react-redux";
 import {getProduct} from "../../../../Redux/reducers/productSlice";
 import LoadingComponent from "../../LoadingComponent";
 import PostUserInfo from "../PostUser/PostUserInfo";
+import {Button, Grid, Header, Icon, Image, Segment} from "semantic-ui-react";
+import StripeCheckoutButton from "../../payment/stripe/StripeCheckOutButton";
 
 const PostDetails = () => {
     const {loading, product} = useSelector(state => state.product);
@@ -21,10 +23,64 @@ const PostDetails = () => {
     if (loading) return <LoadingComponent/>;
 
     return (
-        <div>
-            <PostUserInfo userId={product.user_id}/>
+        <Grid>
+            <Grid.Column width={6}>
+                <Image src={product.post_photo}/>
+            </Grid.Column>
 
-        </div>
+            <Grid.Column width={9}>
+                <Grid.Row>
+                    <Header content={`Price: $ ${product.price}`} as={'h2'}/>
+                    Description
+                </Grid.Row>
+                <Grid.Row>
+                    <Header content={product.description} as={'h3'}/>
+                    {(product.deliveryOptions?.delivery && product.deliveryOptions?.pickup) && (
+                        <>
+                            Delivery: <Icon circular inverted color='teal' name='check'/>
+                            PickUP: <Icon circular inverted color='teal' name='check'/>
+                        </>)}
+
+                    {(product.deliveryOptions?.delivery && !product.deliveryOptions?.pickup) && (
+                        <>
+                            Delivery: <Icon circular inverted color='teal' name='check'/>
+                            PickUP: <Icon circular inverted color='red' name='close'/>
+                        </>
+                    )}
+                    {(!product.deliveryOptions?.delivery && product.deliveryOptions?.pickup) && (
+                        <>
+                            Delivery: <Icon circular inverted color='teal' name='close'/>
+                            PickUP: <Icon circular inverted color='red' name='check'/>
+                        </>
+                    )}
+                    <Header content={`Condition: ${product.condition}`} as={'p'}/>
+                    <Header content={`Created: ${product.createdAt}`} as={'p'}/>
+
+                    {product.barterImage && (<Segment>
+
+                        <>
+                            <Image src={product.barterImage}/>
+                            <header content={`Barter Price :  ${product.barterPrice}`}/>
+                            <header content={`ProductName:  ${product.barterProduct}`}/>
+                            <Button content={'Pay now'} color={'teal'}/>
+                        </>
+
+
+                    </Segment>)}
+
+
+                </Grid.Row>
+
+                <PostUserInfo userId={product.user_id}/>
+                <StripeCheckoutButton price={product.price}/>
+            </Grid.Column>
+
+
+
+
+
+
+        </Grid>
     );
 };
 
